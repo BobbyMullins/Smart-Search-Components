643ea03e4b1e8e3571fab3db8bab65f8e3f4d2ff
Robert Mullins <robertmullins@192.168.1.5>
2017-07-10 19:02:05 -0400

    add v1 of components 2 3 4

---
+++ BA Insight/SS Component 2 - Safety Gauge/Scripts/gauge.js
@@ -0,0 +1,35 @@
+var opts = {
+  angle: -0.2, // The span of the gauge arc
+  lineWidth: 0.05, // The line thickness
+  radiusScale: 1, // Relative radius
+  pointer: {
+    length: 0.3, // // Relative to gauge radius
+    strokeWidth: 0.025, // The thickness
+    color: '#000000' // Fill color
+  },
+  limitMax: false,     // If false, max value increases automatically if value > maxValue
+  limitMin: false,     // If true, the min value of the gauge will be fixed
+  colorStart: '#6FADCF',   // Colors
+  colorStop: '#8FC0DA',    // just experiment with them
+  strokeColor: '#E0E0E0',  // to see which ones work best for you
+  generateGradient: true,
+  highDpiSupport: true,     // High resolution support
+  staticZones: [        //Colored Gauge Regions 
+   {strokeStyle: "#D3D3D3", min: 0, max: 1200}, // Gray
+   {strokeStyle: "#ffff00", min: 1200, max: 1800}, // Yellow
+   {strokeStyle: "#ff9900", min: 1800, max: 2400}, // Orange
+   {strokeStyle: "#ff0000", min: 2400, max: 3000}, // Red
+  ],
+  staticLabels: {       //Gauge Labels 
+  font: "10px sans-serif",  // Specifies font
+  labels: [0, 300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000],  // Print labels at these values
+  color: "#000000",  // Optional: Label text color
+  fractionDigits: 0  // Optional: Numerical precision. 0=round off.
+  },
+};
+var target = document.getElementById('safetyGauge'); // your canvas element
+var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+gauge.maxValue = 3000; // set max gauge value
+gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+gauge.animationSpeed = 32; // set animation speed (32 is default value)
+gauge.set(1250); // set actual value
---
+++ BA Insight/SS Component 4 - Location Map/Scripts/locationMaps.js
@@ -0,0 +1,13 @@
+function initMap() {
+        //need to make lat / lng dynamic 
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
