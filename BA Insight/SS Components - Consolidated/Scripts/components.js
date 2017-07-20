428d69bd733c13741e82d2be78ec4fd6bfbd165c
Robert Mullins <robertmullins@192.168.1.6>
2017-07-20 11:01:49 -0400

    add functions for components 2, 3, 4

--- BA Insight/SS Components - Consolidated/Scripts/components.js
+++ BA Insight/SS Components - Consolidated/Scripts/components.js
@@ -46,97 +46,131 @@ function gaugeInit(mapTarget, mapGauge) {
 /* D3 JS */
 /* ******************************* */
 
-var myData = [100, 125, 320, 440, 500]; 
+function graphInit(chartLocation, yData, xData) {
 
-var margin = {
-    top: 30, 
-    right: 30, 
-    bottom: 40, 
-    left: 50
+    var myData = yData; 
+    var xLabels = xData; 
+
+    var margin = {
+        top: 30, 
+        right: 30, 
+        bottom: 40, 
+        left: 50
+    }
+
+    var height = 500 - margin.top - margin.bottom; 
+    var width = 500 - margin.right - margin.left; 
+    //var barWidth = 35; 
+    //var barOffset = 5; 
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(myData)])
+        .range([0, height]); 
+
+    //var xScale = d3.scale.ordinal()
+    //    .domain(d3.range(0, myData.length))
+    //    .rangeBands([0, width])
+
+    var xScale = d3.scale.ordinal()
+        .domain(d3.range(0, xLabels.length))
+        .rangeBands([0, width], .1)
+
+    d3.select(chartLocation).append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate('+margin.left+','+margin.top+')')
+        .style('background', 'f4f4f4')
+        .selectAll('rect')
+            .data(myData)
+            .enter().append('rect')
+                .style('fill', 'lightgreen')
+                .attr('width', xScale.rangeBand())
+                .attr('height', function(d){
+                    return yScale(d); 
+                })
+                .attr('x', function(d, i){
+                    return xScale(i);  
+                })
+                .attr('y', function(d){
+                    return height - yScale(d); 
+                })
+
+    var vScale = d3.scale.linear()
+        .domain([0, d3.max(myData)])
+        .range([height, 0]); 
+
+    //var hScale = d3.scale.ordinal()
+    //    .domain(d3.range(0, myData.length))
+    //    .rangeBands([0, width])
+    //
+    var hScale = d3.scale.ordinal()
+        .domain(d3.range(0, xLabels.length))
+        .rangeBands([0, width])
+
+    // V Axis
+    var vAxis = d3.svg.axis()
+        .scale(vScale)
+        .orient('left')
+        .ticks(5)
+        .tickPadding(5)
+
+    // V Guide
+    var vGuide = d3.select('svg')
+        .append('g')
+            vAxis(vGuide)
+            vGuide.attr('transform', 'translate('+margin.left+','+margin.top+')')
+            vGuide.selectAll('path')
+                .style('fill', 'none')
+                .style('stroke', '#000')
+            vGuide.selectAll('line')
+                .style('stroke', '#000')
+
+    // H Axis
+    //var hAxis = d3.svg.axis()
+    //    .scale(hScale)
+    //    .orient('bottom')
+    //    .tickValues(hScale.domain().filter(function(d, i){
+    //        return !(i % (myData.length/6)); 
+    //    })); 
+
+    var hAxis = d3.svg.axis()
+        .scale(hScale)
+        .orient('bottom')
+        .tickValues(hScale.domain().filter(function(d, i){
+            return !(i % (xLabels.length/6)); 
+        })); 
+
+    // h Guide
+    var hGuide = d3.select('svg')
+        .append('g')
+            hAxis(hGuide)
+            hGuide.attr('transform', 'translate('+margin.left+','+(height + margin.top)+')')
+            hGuide.selectAll('path')
+                .style('fill', 'none')
+                .style('stroke', '#000')
+            hGuide.selectAll('line')
+                .style('stroke', '#000')
+
 }
-
-var height = 500 - margin.top - margin.bottom; 
-var width = 500 - margin.right - margin.left; 
-//var barWidth = 35; 
-//var barOffset = 5; 
-
-var yScale = d3.scale.linear()
-    .domain([0, d3.max(myData)])
-    .range([0, height]); 
-
-var xScale = d3.scale.ordinal()
-    .domain(d3.range(0, myData.length))
-    .rangeBands([0, width])
-
-d3.select('#chart').append('svg')
-    .attr('width', width + margin.right + margin.left)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate('+margin.left+','+margin.top+')')
-    .style('background', 'f4f4f4')
-    .selectAll('rect')
-        .data(myData)
-        .enter().append('rect')
-            .style('fill', 'lightgreen')
-            .attr('width', xScale.rangeBand())
-            .attr('height', function(d){
-                return yScale(d); 
-            })
-            .attr('x', function(d, i){
-                return xScale(i);  
-            })
-            .attr('y', function(d){
-                return height - yScale(d); 
-            })
-
-var vScale = d3.scale.linear()
-    .domain([0, d3.max(myData)])
-    .range([height, 0]); 
-
-var hScale = d3.scale.ordinal()
-    .domain(d3.range(0, myData.length))
-    .rangeBands([0, width])
-
-// V Axis
-var vAxis = d3.svg.axis()
-    .scale(vScale)
-    .orient('left')
-    .ticks(5)
-    .tickPadding(5)
-
-// V Guide
-var vGuide = d3.select('svg')
-    .append('g')
-        vAxis(vGuide)
-        vGuide.attr('transform', 'translate('+margin.left+','+margin.top+')')
-        vGuide.selectAll('path')
-            .style('fill', 'none')
-            .style('stroke', '#000')
-        vGuide.selectAll('line')
-            .style('stroke', '#000')
-
-// H Axis
-var hAxis = d3.svg.axis()
-    .scale(hScale)
-    .orient('bottom')
-    .tickValues(hScale.domain().filter(function(d, i){
-        return !(i % (myData.length/6)); 
-    })); 
-
-// h Guide
-var hGuide = d3.select('svg')
-    .append('g')
-        hAxis(hGuide)
-        hGuide.attr('transform', 'translate('+margin.left+','+(height + margin.top)+')')
-        hGuide.selectAll('path')
-            .style('fill', 'none')
-            .style('stroke', '#000')
-        hGuide.selectAll('line')
-            .style('stroke', '#000')
         
 /* *************************** */
 /* Maps JS */
 /* *************************** */
+
+function initMap(mapLocation, lat, lng) {
+    
+    var uluru = {lat: lat, lng: lng};
+        var map = new google.maps.Map(document.getElementById(mapLocation), {
+        zoom: 4,
+        center: uluru
+        });
+    
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+        });
+}
         
 //function initMap() {
 //    //first function (initialize map) 
@@ -153,18 +187,6 @@ var hGuide = d3.select('svg')
 //        });
 //      }
 
-function initMap(mapLocation, lat, lng) {
-    
-    var uluru = {lat: lat, lng: lng};
-        var map = new google.maps.Map(document.getElementById(mapLocation), {
-        zoom: 4,
-        center: uluru
-        });
-    
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-        });
-}
+
     
 
