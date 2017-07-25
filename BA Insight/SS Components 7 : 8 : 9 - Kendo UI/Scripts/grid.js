695ae013a05009bb02a878aec10f5dfc606cd5dc
Robert Mullins <robertmullins@192.168.1.6>
2017-07-25 15:40:10 -0400

    add custom functions and dynamic data to Kendo grid

--- BA Insight/SS Components 7 : 8 : 9 - Kendo UI/Scripts/grid.js
+++ BA Insight/SS Components 7 : 8 : 9 - Kendo UI/Scripts/grid.js
@@ -1,74 +1,92 @@
+function chartInit(chartLocation, columnHeaders, data) {
+    
+    for (var i = 0; i<columnHeaders.length; i++) {
+        var field = field; 
+        var title = title; 
+        columns = columnHeaders[i];  
+    }  
+    
+//    for (var j = 0; j<columnHeaders.length; j++) {
+//        dataSource = tableData[i];      
+//    } 
+
     $(document).ready(function(){
         
         
-        $("#grid").kendoGrid({
+        $(chartLocation).kendoGrid({
             sortable: true, 
             pagable: true, 
             filterable: true, 
             
             //Sample Column Names
             
-            columns: [{
-                field: "sampleHeader1",
-                title: "Sample Header 1"
-            },
-            {
-                field: "sampleHeader2",
-                title: "Sample Header 2"
-            }, 
-            {
-                field: 'sampleHeader3', 
-                title: 'Sample Header 3'
-            },
-            {
-                field: 'sampleHeader4', 
-                title: 'Sample Header 4'
-            }, 
-            {
-                field: 'sampleHeader5', 
-                title: 'Sample Header 5'
-            }],
+//            columns: [{
+//                field: "sampleHeader1",
+//                title: "Sample Header 1"
+//            },
+//            {
+//                field: "sampleHeader2",
+//                title: "Sample Header 2"
+//            }, 
+//            {
+//                field: 'sampleHeader3', 
+//                title: 'Sample Header 3'
+//            },
+//            {
+//                field: 'sampleHeader4', 
+//                title: 'Sample Header 4'
+//            }, 
+//            {
+//                field: 'sampleHeader5', 
+//                title: 'Sample Header 5'
+//            }],
+            
+            columns: columnHeaders, 
+              
             
             //Sample Data
             
-            dataSource: {
-                data: [{
-                    sampleHeader1: "xxxxx",
-                    sampleHeader2: "xxxxx", 
-                    sampleHeader3: "xxxxx", 
-                    sampleHeader4: "xxxxx", 
-                    sampleHeader5: "xxxxx"
-                },
-                {
-                    sampleHeader1: "xxxxx",
-                    sampleHeader2: "xxxxx", 
-                    sampleHeader3: "xxxxx", 
-                    sampleHeader4: "xxxxx", 
-                    sampleHeader5: "xxxxx"
-                },
-                {
-                    sampleHeader1: "xxxxx",
-                    sampleHeader2: "xxxxx", 
-                    sampleHeader3: "xxxxx", 
-                    sampleHeader4: "xxxxx", 
-                    sampleHeader5: "xxxxx"
-                },
-                {
-                    sampleHeader1: "xxxxx",
-                    sampleHeader2: "xxxxx", 
-                    sampleHeader3: "xxxxx", 
-                    sampleHeader4: "xxxxx", 
-                    sampleHeader5: "xxxxx"
-                },
-                {
-                    sampleHeader1: "xxxxx",
-                    sampleHeader2: "xxxxx", 
-                    sampleHeader3: "xxxxx", 
-                    sampleHeader4: "xxxxx", 
-                    sampleHeader5: "xxxxx"
-                },
-                ]
-            }
+//            dataSource: {
+//                data: [{
+//                    sampleHeader1: "xxxxx",
+//                    sampleHeader2: "xxxxx", 
+//                    sampleHeader3: "xxxxx", 
+//                    sampleHeader4: "xxxxx", 
+//                    sampleHeader5: "xxxxx"
+//                },
+//                {
+//                    sampleHeader1: "xxxxx",
+//                    sampleHeader2: "xxxxx", 
+//                    sampleHeader3: "xxxxx", 
+//                    sampleHeader4: "xxxxx", 
+//                    sampleHeader5: "xxxxx"
+//                },
+//                {
+//                    sampleHeader1: "xxxxx",
+//                    sampleHeader2: "xxxxx", 
+//                    sampleHeader3: "xxxxx", 
+//                    sampleHeader4: "xxxxx", 
+//                    sampleHeader5: "xxxxx"
+//                },
+//                {
+//                    sampleHeader1: "xxxxx",
+//                    sampleHeader2: "xxxxx", 
+//                    sampleHeader3: "xxxxx", 
+//                    sampleHeader4: "xxxxx", 
+//                    sampleHeader5: "xxxxx"
+//                },
+//                {
+//                    sampleHeader1: "xxxxx",
+//                    sampleHeader2: "xxxxx", 
+//                    sampleHeader3: "xxxxx", 
+//                    sampleHeader4: "xxxxx", 
+//                    sampleHeader5: "xxxxx"
+//                },
+//                ]
+//            }
+            
+            dataSource: data
+            
         });
         
         //formatting for table
@@ -76,3 +94,5 @@
         $('tr:odd').css('background-color', 'white'); 
         $('tr').css('border', '2px solid white'); 
     });
+    
+}
